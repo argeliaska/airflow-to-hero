8431affb68b83508b301f64ba4229b565449bb3e
Argelia Skakie <argeliaska@gmail.com>
2023-10-12 12:36:49 -0600

    [FEAT] push arguments into xcom

--- dags/create_dag_with_python_operator.py
+++ dags/create_dag_with_python_operator.py
@@ -4,11 +4,13 @@ from airflow import DAG
 from airflow.operators.python import PythonOperator
 
 def greet(age, ti):
-    name = ti.xcom_pull(task_ids='get_name')
-    print(f"Hello World! My name is {name} and I am {age} years old!")
+    first_name = ti.xcom_pull(task_ids='get_name', key='first_name')
+    last_name = ti.xcom_pull(task_ids='get_name', key='last_name')
+    print(f"Hello World! My name is {first_name} {last_name} and I am {age} years old!")
 
-def get_name():
-    return 'Alex'
+def get_name(ti): # ti = task instance
+    ti.xcom_push(key='first_name', value='Alex')
+    ti.xcom_push(key='last_name', value='Skakie')
 
 default_args = {
     'owner': 'airflow',
@@ -16,7 +18,7 @@ default_args = {
     'retry_delay': timedelta(minutes=2)
 }
 with DAG(
-    dag_id='our_first_python_dag_v04',
+    dag_id='our_first_python_dag_v05',
     default_args=default_args,
     description='Our first dag using python operator',
     start_date=datetime(2023,10,11),
