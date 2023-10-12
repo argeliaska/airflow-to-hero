42492943e74e5ac850b3f3ef7cd722e968e7c48b
Argelia Skakie <argeliaska@gmail.com>
2023-10-12 12:40:25 -0600

    [FEAT] age added to xcom args and tasks dependency updated

--- dags/create_dag_with_python_operator.py
+++ dags/create_dag_with_python_operator.py
@@ -3,14 +3,18 @@ from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 
-def greet(age, ti):
+def greet(ti):
     first_name = ti.xcom_pull(task_ids='get_name', key='first_name')
     last_name = ti.xcom_pull(task_ids='get_name', key='last_name')
+    age = ti.xcom_pull(task_ids='get_age', key='age')
     print(f"Hello World! My name is {first_name} {last_name} and I am {age} years old!")
 
 def get_name(ti): # ti = task instance
     ti.xcom_push(key='first_name', value='Alex')
     ti.xcom_push(key='last_name', value='Skakie')
+
+def get_age(ti): 
+    ti.xcom_push(key='age', value='20')
 
 default_args = {
     'owner': 'airflow',
@@ -18,7 +22,7 @@ default_args = {
     'retry_delay': timedelta(minutes=2)
 }
 with DAG(
-    dag_id='our_first_python_dag_v05',
+    dag_id='our_first_python_dag_v06',
     default_args=default_args,
     description='Our first dag using python operator',
     start_date=datetime(2023,10,11),
@@ -27,11 +31,16 @@ with DAG(
     task1 = PythonOperator(
         task_id='greet',
         python_callable=greet,
-        op_kwargs={'age':18}
+        # op_kwargs={'age':18}
     )
 
     task2 = PythonOperator(
         task_id='get_name',
         python_callable=get_name
     )
-    task2 >> task1
+
+    task3 = PythonOperator(
+        task_id='get_age',
+        python_callable=get_age
+    )
+    [task2, task3] >> task1
