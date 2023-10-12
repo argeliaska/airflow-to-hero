c4aa6dca51a0298ef60b8c435763d62c45ee8a72
Argelia Skakie <argeliaska@gmail.com>
2023-10-12 14:28:30 -0600

    [FEAT] dag with cron expression interval

---
+++ dags/dag_with_cron_expression.py
@@ -0,0 +1,21 @@
+from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+
+default_args = {
+    'owner': 'airflow',
+    'retries': 5,
+    'retry_delay': timedelta(minutes=5)
+}
+
+with DAG(
+    dag_id='dag_with_cron_expression_v01',
+    default_args=default_args,
+    start_date=datetime(2023,9,30),
+    schedule_interval='0 0 * * *',
+) as dag:
+    task1 = BashOperator(
+        task_id='task1',
+        bash_command="echo DAG with cron expression!"
+    )
