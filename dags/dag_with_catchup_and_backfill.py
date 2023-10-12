6200f9fc11c514d0692195e2578370d00c7e9c14
Argelia Skakie <argeliaska@gmail.com>
2023-10-12 13:13:51 -0600

    [FEAT] dag catchup with backfill commmand

--- dags/dag_with_catchup_and_backfill.py
+++ dags/dag_with_catchup_and_backfill.py
@@ -3,6 +3,11 @@ from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.bash import BashOperator
 
+# the catchup parameter of the DAG was set to False, 
+# but we can still run the DAG in the past using the backfill command
+# In the airflow (scheduler) container execute the command
+# airflow dags backfill with start date, end date and dag id
+# airflow dags backfill -s 2023-10-01 -e 2023-10-07 dag_with_catchup_backfill_v03
 
 default_args = {
     'owner': 'airflow',
@@ -10,11 +15,11 @@ default_args = {
     'retry_delay': timedelta(minutes=5)
 }
 with DAG(
-    dag_id='dag_with_catchup_backfill_v01',
+    dag_id='dag_with_catchup_backfill_v03',
     default_args=default_args,
-    start_date=datetime(2023,10,1),
+    start_date=datetime(2023,9,30),
     schedule_interval='@daily',
-    catchup=True,
+    catchup=False,
 ) as dag:
     task1 = BashOperator(
         task_id='task1',
