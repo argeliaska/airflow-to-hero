e66be3871e277c46b96fa994258a85aeb10ac3ca
Argelia Skakie <argeliaska@gmail.com>
2023-10-12 12:51:36 -0600

    [FEAT] add multiple outputs in a function

--- dags/dag_with_taskflow_api.py
+++ dags/dag_with_taskflow_api.py
@@ -9,26 +9,31 @@ default_args = {
     'retry_delay': timedelta(minutes=5)
 }
 
-@dag(dag_id='dag_with_taskflow_api_v01',
+@dag(dag_id='dag_with_taskflow_api_v02',
      default_args=default_args,
      start_date=datetime(2023,10,11),
     schedule_interval='@daily')
 def hello_world_etl():
 
-    @task()
+    @task(multiple_outputs=True)
     def get_name():
-        return "Jerry"
+        return {
+            'first_name': "Jerry",
+            'last_name': "Fridman",
+        }
     
     @task()
     def get_age():
         return 20
 
     @task()
-    def greet(name, age):
-        print(f"Hello World! My name is {name} and I am {age} years old")
+    def greet(first_name, last_name, age):
+        print(f"Hello World! My name is {first_name} {last_name} and I am {age} years old")
 
-    name = get_name()
+    name_dict = get_name()
     age = get_age()
-    greet(name, age)
+    greet(first_name=name_dict['first_name'], 
+          last_name=name_dict['last_name'], 
+          age=age)
 
 greet_dag = hello_world_etl()
